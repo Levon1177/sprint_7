e3ed945aca24a2a02184c992be66e42a0fe8367e
Levon Petrovskiy <levonpetrovskiy@mail.ru>
2026-06-22 20:42:57 +0300

    ready1

--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCafeWhenOk(t *testing.T) {
@@ -50,5 +52,68 @@ func TestCafeNegative(t *testing.T) {
 }
 
 func TestCafeCount(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
 
+	moscowTotal := len(cafeList["moscow"])
+
+	requests := []struct {
+		count int
+		want  int
+	}{
+		{count: 0, want: 0},
+		{count: 1, want: 1},
+		{count: 2, want: 2},
+		{count: 100, want: moscowTotal},
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", fmt.Sprintf("/cafe?city=moscow&count=%d", v.count), nil)
+		handler.ServeHTTP(response, req)
+
+		require.Equal(t, http.StatusOK, response.Code)
+
+		body := strings.TrimSpace(response.Body.String())
+		var cafes []string
+		if body != "" {
+			cafes = strings.Split(body, ",")
+		}
+
+		assert.Len(t, cafes, v.want)
+	}
+}
+
+func TestCafeSearch(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	requests := []struct {
+		search    string
+		wantCount int
+	}{
+		{search: "фасоль", wantCount: 0},
+		{search: "кофе", wantCount: 2},
+		{search: "вилка", wantCount: 1},
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", fmt.Sprintf("/cafe?city=moscow&search=%s", v.search), nil)
+		handler.ServeHTTP(response, req)
+
+		require.Equal(t, http.StatusOK, response.Code)
+
+		body := strings.TrimSpace(response.Body.String())
+		var cafes []string
+		if body != "" {
+			cafes = strings.Split(body, ",")
+		}
+
+		assert.Len(t, cafes, v.wantCount)
+
+		for _, cafe := range cafes {
+			cafeUpper := strings.ToUpper(cafe)
+			searchUpper := strings.ToUpper(v.search)
+			assert.True(t, strings.Contains(cafeUpper, searchUpper), "Кафе '%s' не содержит переданную строку '%s'", cafe, v.search)
+		}
+	}
 }
